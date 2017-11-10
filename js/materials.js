b55554e170e62f7afef581293334a3b0ec6d0560
SemperDarky <mvidigueira@gmail.com>
2017-11-10 00:33:19 +0000

    quick fixz
    Signed-off-by: SemperDarky <mvidigueira@gmail.com>

--- js/materials.js
+++ js/materials.js
@@ -18,10 +18,10 @@ var CAR_DOME_MATERIAL = [new THREE.MeshPhongMaterial({color: 0x25272b,specular: 
 var CAR_RIMS_MATERIAL = [new THREE.MeshPhongMaterial({color: 0xc0c0c0,specular: 0x020202,shininess: 230}), new THREE.MeshLambertMaterial( {color: 0xc0c0c0})];
 var CAR_HUB_MATERIAL = [new THREE.MeshPhongMaterial({color: 0x808080,specular: 0x020202,shininess: 230}), new THREE.MeshLambertMaterial( {color: 0x808080})];
 var CAR_WHEEL_MATERIAL = [new THREE.MeshPhongMaterial({color: 0x202020,specular: 0x010101,shininess: 10}), new THREE.MeshLambertMaterial( {color: 0x202020})];
-var CAR_BODY_MATERIAL = [new THREE.MeshPhongMaterial({color: 0xaa0000,specular: 0xaa0000,shininess: 150}), new THREE.MeshLambertMaterial( {color: 0xaa0000})];
-var CAR_SPOILSUP_MATERIAL = [new THREE.MeshPhongMaterial({color: 0xaa0000,specular: 0xaa0000,shininess: 150}), new THREE.MeshLambertMaterial( {color: 0xaa0000})];
-var CAR_SPOILTOP_MATERIAL = [new THREE.MeshPhongMaterial({color: 0x25272b,specular: 0x020202,shininess: 400}), new THREE.MeshLambertMaterial( {color: 0x25272b})];
-var CAR_ORNAMENT_MATERIAL = [new THREE.MeshPhongMaterial({color: 0xaaaaaa,specular: 0xaaaaaa,shininess: 1000}), new THREE.MeshLambertMaterial( {color: 0xaaaaaa})];
+var CAR_BODY_MATERIAL = [new THREE.MeshPhongMaterial({color: 0xaa0000,specular: 0x020202,shininess: 100}), new THREE.MeshLambertMaterial( {color: 0xaa0000})];
+var CAR_SPOILSUP_MATERIAL = [new THREE.MeshPhongMaterial({color: 0xaa0000,specular: 0xaa0000,shininess: 100}), new THREE.MeshLambertMaterial( {color: 0xaa0000})];
+var CAR_SPOILTOP_MATERIAL = [new THREE.MeshPhongMaterial({color: 0x25272b,specular: 0x020202,shininess: 100}), new THREE.MeshLambertMaterial( {color: 0x25272b})];
+var CAR_ORNAMENT_MATERIAL = [new THREE.MeshPhongMaterial({color: 0xaaaaaa,specular: 0x020202,shininess: 800}), new THREE.MeshLambertMaterial( {color: 0xaaaaaa})];
 
 var CANDLE_MATERIAL = [new THREE.MeshPhongMaterial({color: 0xaa0000,specular: 0x020202,shininess: 180}), new THREE.MeshLambertMaterial( {color: 0xaa0000})];
 var CANDLE_BASE_MATERIAL = [new THREE.MeshPhongMaterial({color: 0xaaaaaa,specular: 0x020202,shininess: 230}), new THREE.MeshLambertMaterial( {color: 0xaaaaaa})];
